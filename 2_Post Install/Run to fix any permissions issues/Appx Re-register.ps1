70e7ef5f497a2a89800c3e9bfcce8153168956f0
gex581990 <129348281+gex581990@users.noreply.github.com>
2024-02-13 01:29:56 -0500

    Update Appx Re-register.ps1
    Added ability to turn off Start and Shell experience so they can properly re-register

--- 2_Post Install/Run to fix any permissions issues/Appx Re-register.ps1
+++ 2_Post Install/Run to fix any permissions issues/Appx Re-register.ps1
@@ -1,1 +1,6 @@
+Stop-Process -Name "StartMenuExperienceHost" -Force
+Stop-Process -Name "ShellExperienceHost" -Force
+
 Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+
+Read-Host -Prompt "Press Enter to exit"
